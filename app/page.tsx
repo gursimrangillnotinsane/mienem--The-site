e6863261dd27c298cd5dd8b91a0ba73b26f83408
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-10-31 17:30:06 -0400

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
-'use client';
-import client from "../../mienem_api/lib/client";
-import { ApolloProvider } from '@apollo/client'
-import Openerpage from '../components/opener.page'
+
 import MainHeading from '../components/mainheading.page'
-import { NextPage } from 'next';
-import React from "react";
-import { Main } from "next/document";
+
 
 const Page = () => {
   return <>
     <div>
-
-
-      {/* <Openerpage /> */}
       <MainHeading />
     </div>
   </>
