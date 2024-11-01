ce0f5b0a432d1070d6cda88fa4a5c79f3df06e5d
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-11-01 18:17:19 -0400

    tracking3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import mixpanel from "mixpanel-browser";
+// app/layout.js
+import Script from 'next/script'
 
 
 
@@ -15,28 +16,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // Near entry of your product, init Mixpanel
-  mixpanel.init("f5d1fdb7e92b09cb5c9720165c0b893f", {
-    debug: true,
-    track_pageview: true,
-    persistence: "localStorage",
-  });
+
 
   return (
     <html lang="en">
-      {/* <head>
-        <script type="text/javascript">
-          {`
-          (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "oryas4n2ws");
-    `}
-        </script>
-      </head> */}
       <body>
         {children}
+        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
       </body>
     </html>
   );
