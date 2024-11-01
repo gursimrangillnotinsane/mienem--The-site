d406426a09819892524822e32952a967284f07ce
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-11-01 18:00:12 -0400

    tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+      <head>
+        <script type="text/javascript">
+          {`
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "oryas4n2ws");
+    `}
+        </script>
+      </head>
       <body>
         {children}
       </body>
